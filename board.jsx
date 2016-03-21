cbef22bbc1a7cc7632fdc40209b9fc785cc4a1a1
Adam <ebokianknight@gmail.com>
2016-03-21 15:28:11 -0400

    Tile Logic

--- board.jsx
+++ board.jsx
@@ -7,16 +7,16 @@ var Board = React.createClass({
 
   render:  function () {
     var tiles = [];
+    var that = this;
     for (var i = 0; i < this.props.board.grid.length; i++) {
       for (var j = 0; j < this.props.board.grid[0].length; j++) {
         tiles.push(this.props.board.grid[i][j]);
       }
     }
-    debugger;
     return (
       <ul className="grid group">
-        { tiles.map( function (tile){
-          return <Tile tile={tile}/>;
+        { tiles.map( function (tile, index){
+          return <Tile key={index} tile={tile} updateGame={that.props.updateGame}/>;
         })}
       </ul>
     );
--- tile.jsx
+++ tile.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
 
 var Tile = React.createClass({
+  handleClick: function () {
+
+  },
 
   render:  function () {
+    var tile = this.props.tile;
+    var flagged = tile.flagged ? "flagged " : "";
+    var explored = tile.explored ? "explored " : "";
+    var klass = "tile " + flagged + explored;
+    var bombs = tile.explored ? tile.adjacentBombCount() : tile.adjacentBombCount();
+
     return (
-        <li className="tile"></li>
+      <li className={klass} onClick={this.handleClick}>{bombs}</li>
     );
   }
 
